aecae269ed60079826820756cd9e8e8f304fdf35
indiirazhr <indirazah18@gmail.com>
2022-11-21 22:20:57 +0700

    first commit

---
+++ latihan2.php
@@ -0,0 +1,38 @@
+<?php 
+
+// echo "1"; //untuk menampilkan data atau tulisan ke layar
+
+//$latihan = "awikwok";
+
+//echo $latihan;
+
+//$aku = "indira";
+
+//echo $aku;
+
+//$a = "gula";
+//$b = 5;
+
+// echo $a*$b;
+
+//var_dump($b)//untuk melihat tipe data
+
+//$a = true; //boolean true or false
+
+//var_dump($a);
+
+$gula = ["merah", 1 , "putih", 4]; //fungsi array untuk menampung banyak data 
+//var_dump($gula);
+
+//echo $gula[1]; //menampilkan data dengan memanggil indeksnya (nomor antrian)
+
+foreach ($gula as $uyah) { //menampilkan seluruh data yang ada di array
+	echo $uyah. "<br>";
+}
+
+
+
+
+
+
+ ?>
---
+++ percabangan.php
@@ -0,0 +1,29 @@
+<?php
+
+$angka = $_POST["nilai"];
+
+
+
+
+
+
+
+
+if($angka == "a"){
+	echo "Sangat Baik";
+}elseif($angka == "b"){
+	echo "Baik";
+}elseif($angka == "c"){
+	echo "Cukup";
+}else{
+	echo "GAGAL";
+
+}
+
+
+
+
+
+
+
+?>
